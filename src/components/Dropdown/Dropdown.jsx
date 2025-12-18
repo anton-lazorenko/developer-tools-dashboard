38dc11d81aa8a07e4c42f9091de57fe436a5d433
Anton Lazorenko <153720712+anton-lazorenko@users.noreply.github.com>
2025-12-18 18:58:27 +0200

    add dropdown menu for sidebar component

---
+++ src/components/Dropdown/Dropdown.jsx
@@ -0,0 +1,27 @@
+import styles from './dropdown.module.scss';
+
+import { useState } from "react";
+import dropDownArrow from '../../assets/icons/dropDownArrow.svg';
+
+export default function Dropdown({ title, children, defaultOpen = false }) {
+  const [open, setOpen] = useState(defaultOpen);
+
+  return (
+    <div className={styles.dropdown}>
+      <button
+        className={styles.dropdown__trigger}
+        onClick={() => setOpen(open => !open)}
+      >
+        {title}
+        <span className={`${styles.arrow} ${open ? styles.open : ""}`}>
+          <img src={dropDownArrow} width={20} height={20} fill='white' alt="copy" />
+        </span>
+      </button>
+      {open && (
+        <div className={styles.dropdown__content}>
+          {children}
+        </div>
+      )}
+    </div>
+  );
+}
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,14 +1,21 @@
 import styles from './sidebar.module.scss';
 import { NavLink } from "react-router-dom";
+import Dropdown from '../Dropdown/Dropdown';
 
 export function Sidebar() {
   return (
     <aside className={styles.sidebar}>
       <nav>
         <ul className={styles.list}>
-          <li><NavLink to='/generators' className={({ isActive }) => isActive ? styles.active : styles.default}>Generators</NavLink></li>
-          <li><NavLink to='/encoders' className={({ isActive }) => isActive ? styles.active : styles.default}>Encoders/Decoders</NavLink></li>
-          <li><NavLink to='/converters' className={({ isActive }) => isActive ? styles.active : styles.default}>Converters</NavLink></li>
+          <Dropdown title='Generators'>
+            <li><NavLink to='/generators' className={({ isActive }) => isActive ? styles.active : styles.default}>Generators</NavLink></li>
+          </Dropdown>
+          <Dropdown title='Encoders'>
+            <li><NavLink to='/encoders' className={({ isActive }) => isActive ? styles.active : styles.default}>Encoders/Decoders</NavLink></li>
+          </Dropdown>
+          <Dropdown title='Converters'>
+            <li><NavLink to='/converters' className={({ isActive }) => isActive ? styles.active : styles.default}>Converters</NavLink></li>
+          </Dropdown>
         </ul>
       </nav>
     </aside>
